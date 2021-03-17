c0866989090e40f1eb4368ce2b094981c7320a60
youme <mrsimplelife@naver.com>
2021-03-17 13:59:58 +0900

    scroll

--- js/window-coordinates.js
+++ js/window-coordinates.js
@@ -5,3 +5,24 @@ special.addEventListener("click", (event) => {
   console.log(`${event.clientX},${event.clientY}`);
   console.log(`${event.pageX},${event.pageY}`);
 });
+
+const scrollBy = document.querySelector(".scroll-by");
+const scrollTo = document.querySelector(".scroll-to");
+const scrollInto = document.querySelector(".scroll-into");
+scrollBy.addEventListener("click", () => {
+  window.scrollBy({
+    behavior: "smooth",
+    left: 0,
+    top: 100,
+  });
+});
+scrollTo.addEventListener("click", () => {
+  window.scrollTo({
+    behavior: "smooth",
+    left: 0,
+    top: 100,
+  });
+});
+scrollInto.addEventListener("click", () => {
+  special.scrollIntoView({ behavior: "smooth", block: "center" });
+});
