7c1add3bec8e961fd8c7c2b14d0bbd390a3b0f9a
youme <mrsimplelife@naver.com>
2021-03-17 18:10:37 +0900

    template

--- js/dom.js
+++ js/dom.js
@@ -5,3 +5,10 @@ h2.textContent = "This is a title";
 section.append(h2);
 const h3 = document.querySelector("h3");
 section.insertBefore(h2, h3);
+
+section.removeChild(h3);
+section.innerHTML = `
+<img src="img/avatar.png" alt="avatar" />
+<h1 id="brand">Dream Coding</h1>
+<h3>Don't forget to code your dream</h3>
+`;
